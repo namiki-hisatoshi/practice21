ee1509decdd22589da60b9df4f22fd5422c0afc4
並木　久稔 <namikihisatoshi@namikihisatoshinoMacBook-Pro.local>
2021-01-20 23:09:05 +0900

    refactoring

--- practice.rb
+++ practice.rb
@@ -6,36 +6,35 @@ puts <<~TEXT
   3. 九州旅行(15000円)
 TEXT
 
-sum = 0
 while true
   print "プラン番号を選択 > "
   plan_num = gets.to_i
-  break if plan_num == 1 or 2 or 3
+  break if(1..3).include?(plan_num)
     puts "1~3の番号を入力してください。"
 end
 case plan_num
   when 1
-    sum += 10000
-    plan_name = "沖縄旅行"
+    price = 10000
+    place = "沖縄旅行"
   when 2
-    sum += 20000
-    plan_name = "北海道旅行"
+    price = 20000
+    place = "北海道旅行"
   when 3
-    sum += 15000
-    plan_name = "九州旅行"
+    price = 15000
+    place = "九州旅行"
 end
-puts "#{plan_name}ですね。"
+puts "#{place}ですね。"
 
 puts "何名で予約されますか？"
 while true
   print "人数を入力 > "
   number_of_people = gets.to_i
-  break if(number_of_people.to_i > 0)
+  break if(number_of_people > 0)
     puts "1以上を入力してください。"
   end
 puts "#{number_of_people}名ですね。"
 
-total_price = sum * number_of_people
+total_price = price * number_of_people
 if number_of_people >= 5
   total_price *= 0.9
   puts "5名以上ですので10%割引となります"
